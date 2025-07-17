cd773847911be5528ec1c59988ad7742789efa0a
katerina99 <katerinaioannidou99@gmail.com>
2025-07-17 09:48:58 +0300

    db

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 // db.js
-require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  user: 'kioann12',
+  host: 'database-1.crykwk22g7ax.eu-north-1.rds.amazonaws.com',
+  database: 'hoteldatabase',
+  password: '1056139Katerin',
+  port: 5432, // or your actual port
   ssl: {
     rejectUnauthorized: false // Required for RDS connections
   }
